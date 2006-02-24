8b71aa7082d9e976e05e64816f04c19a6ea2c74b
keelia <none@none>
2006-02-24 00:28:54 +0000

    initial checkin of 2.1 work.

---
+++ doc/XmlHelp.hs
@@ -0,0 +1,22 @@
+<?xml version='1.0' encoding='ISO-8859-1' ?>
+<!DOCTYPE helpset
+  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN"
+         "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
+
+<helpset version="1.0">
+
+  <!-- title -->
+  <title>Xena - Help</title>
+  <!-- maps -->
+  <maps>
+     <homeID>top</homeID>
+     <mapref location="xena/plugin/xml/Map.jhm"/>
+  </maps>
+  <view mergetype="javax.help.UniteAppendMerge">
+    <name>TOC</name>
+    <label>Table Of Contents</label>
+    <type>javax.help.TOCView</type>
+    <data>xena/plugin/xml/TOC.xml</data>
+  </view>
+
+</helpset>
