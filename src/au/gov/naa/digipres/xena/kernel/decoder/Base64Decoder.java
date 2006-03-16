b6fa31641db0149927e493c25e25121d91174a25
keelia <none@none>
2006-03-16 01:50:23 +0000

    added package! decoder now has its own package and base 64 encode has been assimilated into xena.

---
+++ src/au/gov/naa/digipres/xena/kernel/decoder/Base64Decoder.java
@@ -0,0 +1,41 @@
+package au.gov.naa.digipres.xena.kernel.decoder;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import au.gov.naa.digipres.xena.kernel.XenaInputSource;
+
+/**
+ * Decoder for the base64 type
+ *
+ * @author  Chris Bitmead
+ */
+public class Base64Decoder extends Decoder {
+	/**
+	 *
+	 */
+	public Base64Decoder() {
+	}
+
+	/**
+	 *
+	 */
+	public String getName() {
+		return "Base64";
+	}
+
+	/**
+	 * @param  source  stream for decoding
+	 * @return         decoded stream
+	 */
+
+	public XenaInputSource decode(XenaInputSource source) throws IOException {
+		sun.misc.BASE64Decoder decoder = new sun.misc.BASE64Decoder();
+		File tmpFile = File.createTempFile("xenadec", "dat");
+        XenaInputSource rtn = new XenaInputSource(tmpFile);
+		rtn.setTmpFile(true);
+		FileOutputStream os = new FileOutputStream(tmpFile);
+		decoder.decodeBuffer(source.getByteStream(), os);
+		return rtn;
+	}
+}
---
+++ src/au/gov/naa/digipres/xena/kernel/decoder/RawDecoder.java
@@ -0,0 +1,21 @@
+package au.gov.naa.digipres.xena.kernel.decoder;
+import au.gov.naa.digipres.xena.kernel.XenaInputSource;
+
+/**
+ * Standard decoder. Merely passes its input to the output. This is for when
+ * data is not encoded at all.
+ *
+ * @author Chris Bitmead
+ */
+public class RawDecoder extends Decoder {
+	public RawDecoder() {
+	}
+
+	public String getName() {
+		return "Raw";
+	}
+
+	public XenaInputSource decode(XenaInputSource inputSource) {
+		return inputSource;
+	}
+}
