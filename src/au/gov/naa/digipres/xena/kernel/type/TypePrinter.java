194f04c9a77ca9688a80cfe2575a36c83ae23f4b
keelia <none@none>
2006-03-16 02:07:08 +0000

    added reference to type manager - no more singleton.

--- src/au/gov/naa/digipres/xena/kernel/type/TypePrinter.java
+++ src/au/gov/naa/digipres/xena/kernel/type/TypePrinter.java
@@ -6,7 +6,24 @@ import javax.print.Doc;
 import au.gov.naa.digipres.xena.kernel.XenaException;
 
 abstract public class TypePrinter {
-	abstract public XenaFileType getType() throws XenaException;
+    
+    protected TypePrinterManager typePrinterManager;
+    
+	/**
+     * @return Returns the typePrinterManager.
+     */
+    public TypePrinterManager getTypePrinterManager() {
+        return typePrinterManager;
+    }
+
+    /**
+     * @param typePrinterManager The new value to set typePrinterManager to.
+     */
+    public void setTypePrinterManager(TypePrinterManager typePrinterManager) {
+        this.typePrinterManager = typePrinterManager;
+    }
+
+    abstract public XenaFileType getType() throws XenaException;
 
 	abstract public Doc getDoc(File file) throws XenaException;
 }
