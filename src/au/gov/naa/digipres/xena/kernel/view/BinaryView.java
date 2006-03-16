39019a51cd18313f5c44d7f7413b4c671c1c4904
keelia <none@none>
2006-03-16 02:03:41 +0000

    binary normaliser assimiliated into Xena. also refactored and updated to remove singleton references.

---
+++ src/au/gov/naa/digipres/xena/kernel/view/BinaryView.java
@@ -0,0 +1,43 @@
+package au.gov.naa.digipres.xena.kernel.view;
+import java.awt.BorderLayout;
+
+import javax.swing.JLabel;
+
+import au.gov.naa.digipres.xena.kernel.PluginManager;
+import au.gov.naa.digipres.xena.kernel.XenaException;
+import au.gov.naa.digipres.xena.kernel.type.TypeManager;
+import au.gov.naa.digipres.xena.kernel.type.XenaBinaryFileType;
+
+/**
+ * View for random binary files. Due to the nature of binary files, there isn't
+ * any particularly useful view for them, so this view is very basic. It would
+ * be nice if it was enhanced to perhaps show octal values or something like
+ * Unix od.
+ *
+ * @author Chris Bitmead
+ */
+public class BinaryView extends XenaView {
+	private JLabel jLabel1 = new JLabel();
+
+	public BinaryView() {
+		try {
+			jbInit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public boolean canShowTag(String tag) throws XenaException {
+		return tag.equals(viewManager.getPluginManager().getTypeManager().lookupXenaFileType(XenaBinaryFileType.class).getTag());
+	}
+
+	private void jbInit() throws Exception {
+		jLabel1.setText("Binary Data");
+		this.add(jLabel1, BorderLayout.NORTH);
+	}
+
+	public String getViewName() {
+		return "Binary View";
+	}
+
+}
