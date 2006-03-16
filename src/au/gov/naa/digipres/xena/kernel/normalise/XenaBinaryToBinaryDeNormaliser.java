9a7d7ade04899cc724cf27a224eaac50b139fcfc
keelia <none@none>
2006-03-16 01:59:10 +0000

    binary normaliser has been assimilated into xena.

---
+++ src/au/gov/naa/digipres/xena/kernel/normalise/XenaBinaryToBinaryDeNormaliser.java
@@ -0,0 +1,22 @@
+package au.gov.naa.digipres.xena.kernel.normalise;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+
+import au.gov.naa.digipres.xena.helper.BinaryDeNormaliser;
+
+/**
+ * Denormaliser for converting a Xena binary-object file to a plain binary object.
+ *
+ * @author Chris Bitmead
+ */
+public class XenaBinaryToBinaryDeNormaliser extends BinaryDeNormaliser {
+	public void startElement(String namespaceURI, String localName, String qName, Attributes atts) throws SAXException {
+		if (qName.equals("binary-object:binary-object")) {
+			start();
+		}
+	}
+    
+    public String toString(){
+        return "XenaBinary";
+    }
+}
