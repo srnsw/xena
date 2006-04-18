8827afb1e536bfdc2825547a9b9af382f99a4e6a
keelia <none@none>
2006-04-18 04:42:19 +0000

    +updated imports to reflect package helper turning into util.

--- src/au/gov/naa/digipres/xena/plugin/multipage/MultiPageView.java
+++ src/au/gov/naa/digipres/xena/plugin/multipage/MultiPageView.java
@@ -11,10 +11,10 @@ import org.xml.sax.Attributes;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 
-import au.gov.naa.digipres.xena.helper.ChunkedView;
-import au.gov.naa.digipres.xena.helper.XmlContentHandlerSplitter;
 import au.gov.naa.digipres.xena.kernel.XenaException;
 import au.gov.naa.digipres.xena.kernel.view.XenaView;
+import au.gov.naa.digipres.xena.util.ChunkedView;
+import au.gov.naa.digipres.xena.util.XmlContentHandlerSplitter;
 import au.gov.naa.digipres.xena.kernel.view.XmlDivertor;
 
 /**
