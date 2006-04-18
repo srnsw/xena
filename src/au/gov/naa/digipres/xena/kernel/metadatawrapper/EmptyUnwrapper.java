7283bf2832c3312d68b1db3debac78984ca2a3d5
keelia <none@none>
2006-04-18 02:07:25 +0000

    added code to make this work.

--- src/au/gov/naa/digipres/xena/kernel/metadatawrapper/EmptyUnwrapper.java
+++ src/au/gov/naa/digipres/xena/kernel/metadatawrapper/EmptyUnwrapper.java
@@ -1,5 +1,7 @@
 package au.gov.naa.digipres.xena.kernel.metadatawrapper;
 // SAX classes.
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
 import org.xml.sax.helpers.XMLFilterImpl;
 
 /**
@@ -11,5 +13,28 @@ import org.xml.sax.helpers.XMLFilterImpl;
  * @author aak
  */
 public class EmptyUnwrapper extends XMLFilterImpl {
-	
+    boolean contentFound = true;
+
+
+    public void startElement(String namespaceURI, String localName,
+                             String qName, Attributes atts) throws SAXException {
+        
+      super.startElement(namespaceURI, localName, qName, atts);            
+        
+    }
+
+    public void endElement(String namespaceURI, String localName, String qName) throws
+        SAXException {
+            super.endElement(namespaceURI, localName, qName);
+        
+    }
+
+    public void characters(char[] ch, int start, int length) throws
+        SAXException {
+            super.characters(ch, start, length);
+    }
+
+    protected boolean pass() {
+        return contentFound;
+    }
 }
