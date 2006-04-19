f66a01a14a210373898452d33bd3ac3c7e2b8af0
keelia <none@none>
2006-04-19 05:31:23 +0000

    refactored and some reqorking went on. By and large the code was changed to take more of the burden of managing file naming from the actual file namers.

--- src/au/gov/naa/digipres/xena/kernel/filenamer/FileNamerManager.java
+++ src/au/gov/naa/digipres/xena/kernel/filenamer/FileNamerManager.java
@@ -1,15 +1,21 @@
 package au.gov.naa.digipres.xena.kernel.filenamer;
+
+import java.io.File;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.xml.sax.XMLReader;
+
 import au.gov.naa.digipres.xena.javatools.JarPreferences;
 import au.gov.naa.digipres.xena.javatools.PluginLoader;
 import au.gov.naa.digipres.xena.kernel.LoadManager;
 import au.gov.naa.digipres.xena.kernel.PluginManager;
 import au.gov.naa.digipres.xena.kernel.XenaException;
+import au.gov.naa.digipres.xena.kernel.XenaInputSource;
+import au.gov.naa.digipres.xena.kernel.normalise.AbstractNormaliser;
 import au.gov.naa.digipres.xena.kernel.normalise.NormaliserManager;
 
 /**
@@ -17,38 +23,32 @@ import au.gov.naa.digipres.xena.kernel.normalise.NormaliserManager;
  * FileNamer. It is very easy for third parties to write their own FileNamers
  * and the user can choose which one to use from the Properties menu item.
  * <cough>bullshit</cough>
- *
- * @see FileNamer
+ * 
+ * @see AbstractFileNamer
  * @author Chris Bitmead
  */
 public class FileNamerManager implements LoadManager {
-	/**
-	 * String className -> FileNamer
-	 */
-	protected Map<String, FileNamer> namers = new HashMap<String, FileNamer>();
+    /**
+     * String className -> FileNamer
+     */
+    protected Map<String, AbstractFileNamer> namers = new HashMap<String, AbstractFileNamer>();
 
-	public final static String FILE_NAMER_PREF = "fileNamer";
-
-    
-//	static FileNamerManager theSingleton = new FileNamerManager();
-//	
-//	public static FileNamerManager singleton() {
-//	    return theSingleton;
-//	}
+    public static final String DEFAULT_EXTENSION = "xena";
 
     private PluginManager pluginManager;
-    
-    private FileNamer activeFileNamer;
-    
-    
-	public FileNamerManager(PluginManager pluginManager) {
-	    this.pluginManager = pluginManager;
-        FileNamer defaultNamer = new DefaultFileNamer();
+
+    private AbstractFileNamer activeFileNamer;
+
+    private File destinationDir;
+
+    public FileNamerManager(PluginManager pluginManager) {
+        this.pluginManager = pluginManager;
+        AbstractFileNamer defaultNamer = new DefaultFileNamer();
         activeFileNamer = defaultNamer;
         namers.put(defaultNamer.getClass().getName(), defaultNamer);
-	}
+    }
 
-	/**
+    /**
      * @return Returns the pluginManager.
      */
     public PluginManager getPluginManager() {
@@ -56,75 +56,83 @@ public class FileNamerManager implements LoadManager {
     }
 
     /**
-     * @param pluginManager The new value to set pluginManager to.
+     * @param pluginManager
+     *            The new value to set pluginManager to.
      */
     public void setPluginManager(PluginManager pluginManager) {
         this.pluginManager = pluginManager;
     }
 
     public boolean load(JarPreferences preferences) throws XenaException {
-		try {
-			PluginLoader loader = new PluginLoader(preferences);
-			List transes = loader.loadInstances("fileNamers");
-			Iterator it = transes.iterator();
+        try {
+            PluginLoader loader = new PluginLoader(preferences);
+            List transes = loader.loadInstances("fileNamers");
+            Iterator it = transes.iterator();
 
-			while (it.hasNext()) {
-				FileNamer namer = (FileNamer)it.next();
-				namers.put(namer.getClass().getName(), namer);
-                
-                if ( activeFileNamer.getClass().equals(DefaultFileNamer.class) ) {
+            while (it.hasNext()) {
+                AbstractFileNamer namer = (AbstractFileNamer) it.next();
+                namer.setFileNamerManager(this);
+                namers.put(namer.getClass().getName(), namer);
+
+                if (activeFileNamer.getClass().equals(DefaultFileNamer.class)) {
                     activeFileNamer = namer;
                 }
             }
-			return!transes.isEmpty();
-		} catch (ClassNotFoundException e) {
-			throw new XenaException(e);
-		} catch (IllegalAccessException e) {
-			throw new XenaException(e);
-		} catch (InstantiationException e) {
-			throw new XenaException(e);
-		}
-	}
+            return !transes.isEmpty();
+        } catch (ClassNotFoundException e) {
+            throw new XenaException(e);
+        } catch (IllegalAccessException e) {
+            throw new XenaException(e);
+        } catch (InstantiationException e) {
+            throw new XenaException(e);
+        }
+    }
 
-	/**
-	 * Get all the available File Namers.
-	 * @return Collection of File Namers
-	 */
-	public Collection<FileNamer> getFileNamers() {
-		return namers.values();
-	}
+    /**
+     * Get all the available File Namers.
+     * 
+     * @return Collection of File Namers
+     */
+    public Collection<AbstractFileNamer> getFileNamers() {
+        return namers.values();
+    }
 
     /**
      * Get a list of the name of the loaded file namers
-     * @return Collection of File Namer names 
+     * 
+     * @return Collection of File Namer names
      */
     public Collection<String> getListOfNamers() {
         return namers.keySet();
     }
-    
-	/**
-	 * Find a FileNamer object given its class name.
-	 * @param name class name
-	 * @return FileNamer
-	 */
-	public FileNamer lookupByClassName(String name) {
-		return namers.get(name);
-	}
+
+    /**
+     * Find a FileNamer object given its class name.
+     * 
+     * @param name
+     *            class name
+     * @return FileNamer
+     */
+    public AbstractFileNamer lookupByClassName(String name) {
+        return namers.get(name);
+    }
 
     /**
      * return the Active file namer.
+     * 
      * @return FileNamer
      */
-    public FileNamer getActiveFileNamer(){
+    public AbstractFileNamer getActiveFileNamer() {
         return activeFileNamer;
     }
-    
+
     /**
      * Set active filenamer to be specified filenamer. return true on success.
+     * 
      * @param fileNamer
      * @return boolean
      */
-    public boolean setActiveFileNamer(FileNamer fileNamer){
+    public boolean setActiveFileNamer(AbstractFileNamer fileNamer) {
         if (fileNamer != null) {
             activeFileNamer = fileNamer;
             return true;
@@ -134,35 +142,59 @@ public class FileNamerManager implements LoadManager {
 
     /**
      * Set active filenamer by name. return true on success.
+     * 
      * @param fileNamer
      * @return boolean
      */
     public boolean setActiveFileNamer(String name) {
-        if (namers.get(name) != null){
+        if (namers.get(name) != null) {
             activeFileNamer = namers.get(name);
             return true;
         }
         return false;
     }
-    
-    
-	/**
-     * TODO: This should really be removed. The 'prefs' should be handled by the GUI
-     * or whatever app needs them...
-	 * Get the FileNamer that has currently been configured by the user.
-	 * @return FileNamer
-     * @deprecated
-	 */
-    @Deprecated
-	public FileNamer getFileNamerFromPrefs() {
-		JarPreferences root = (JarPreferences)JarPreferences.userNodeForPackage(NormaliserManager.class);
-		String fileNamerName = root.get(FILE_NAMER_PREF, "");
-		if (!fileNamerName.equals("")) {
-			FileNamer en = namers.get(fileNamerName);
-			return en;
-		}
-		return null;
-	}
 
-	public void complete() {}
+    public void complete() {
+    }
+
+    /**
+     * Return the destination directory.
+     * If destinationDir has been set just return destinationDir. If
+     *  it has not been initialised, get the current working directory
+     *  from the system properties, return that. If there is a problem
+     *  with this for some reason, return null.
+     * 
+     * @return Returns the destinationDirectory. It may return null
+     * if the destinationDir is not initialised and the user.dir system propery
+     * is not set or incorrect for some reason.
+     */
+    public File getDestinationDir() {
+        if (destinationDir != null) {
+            return destinationDir;
+        }
+        // return new File(System.getProperty("user.dir"));
+        String currentDirectoryString = System.getProperty("user.dir");
+        if (currentDirectoryString != null) {
+            File currentDirectory = new File(currentDirectoryString);
+            if (currentDirectory.exists() && currentDirectory.isDirectory()) {
+                destinationDir = currentDirectory;
+                return currentDirectory;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @param destinationDirectory
+     *            The new value to set destinationDirectory to.
+     */
+    public void setDestinationDir(File destinationDirectory) {
+        if (!destinationDirectory.exists()
+                || !destinationDirectory.isDirectory()) {
+            throw new IllegalArgumentException(
+                    "Destination directory must be a valid directory!");
+        }
+        this.destinationDir = destinationDirectory;
+    }
+
 }
