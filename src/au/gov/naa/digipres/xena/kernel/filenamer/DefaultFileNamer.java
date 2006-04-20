a6de34a56a8960c6092311d2749ec92a4d560c6b
keelia <none@none>
2006-04-20 07:15:53 +0000

    added really really bad code to ensure uniqueness for generated files.

--- src/au/gov/naa/digipres/xena/kernel/filenamer/DefaultFileNamer.java
+++ src/au/gov/naa/digipres/xena/kernel/filenamer/DefaultFileNamer.java
@@ -7,20 +7,14 @@ package au.gov.naa.digipres.xena.kernel.filenamer;
 
 import java.io.File;
 import java.io.FileFilter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.xml.sax.XMLReader;
-
-import au.gov.naa.digipres.xena.kernel.LegacyXenaCode;
 import au.gov.naa.digipres.xena.kernel.XenaException;
 import au.gov.naa.digipres.xena.kernel.XenaInputSource;
 import au.gov.naa.digipres.xena.kernel.normalise.AbstractNormaliser;
-import au.gov.naa.digipres.xena.kernel.normalise.NormaliserManager;
 
 /**
  * @author andrek24 created 29/09/2005 xena Short desc of class:
@@ -50,17 +44,23 @@ public class DefaultFileNamer extends AbstractFileNamer {
         File newXenaFile = null;
         if (destinationDir != null) {
             id = getId(input, normaliser.toString());
-            newXenaFile = new File(destinationDir, id + "." + FileNamerManager.DEFAULT_EXTENSION);
+            String fileName = id + "." + FileNamerManager.DEFAULT_EXTENSION;
+            newXenaFile = new File(destinationDir, fileName);
+            int i = 1;
+            DecimalFormat idFormatter = new DecimalFormat("0000");
+            while (newXenaFile.exists()) {
+                fileName = id + idFormatter.format(i) + "." + FileNamerManager.DEFAULT_EXTENSION;
+                newXenaFile = new File(destinationDir, fileName);
+                i++;
+            }
         }
         return newXenaFile;
     }
 
     private String getId(XenaInputSource input, String normaliserName) {
-        // generate the name for this file.
-       
-        
+        // generate the id for this file.
         String systemId = input.getSystemId();
-
+        
         //we really only want everything after the last '\' or '/'
         // this should really be fixed up. right now i want lunch so this will be quick and dirty.
         // heheh this is poxy. my code is teh sux00rs
@@ -81,8 +81,6 @@ public class DefaultFileNamer extends AbstractFileNamer {
                 }
             }
         }
-        // now add it to the nameMap that all file namers must keep up to date...
-        addToNameMap(input, newName);
         return newName;
     }
     
