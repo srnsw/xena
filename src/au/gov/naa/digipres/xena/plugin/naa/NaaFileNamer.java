c755bc62dfc6ed78782819b4a2b481bce6d81454
keelia <none@none>
2006-04-27 04:18:08 +0000

    cleaned up imports, removed busted / deprecated code

--- src/au/gov/naa/digipres/xena/plugin/naa/NaaFileNamer.java
+++ src/au/gov/naa/digipres/xena/plugin/naa/NaaFileNamer.java
@@ -1,31 +1,17 @@
 package au.gov.naa.digipres.xena.plugin.naa;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileFilter;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.StringTokenizer;
-import java.util.logging.Logger;
 import java.util.zip.CRC32;
 
-import org.xml.sax.XMLReader;
-
-import au.gov.naa.digipres.xena.javatools.SimpleFileFilter;
-import au.gov.naa.digipres.xena.kernel.LegacyXenaCode;
 import au.gov.naa.digipres.xena.kernel.MultiInputSource;
 import au.gov.naa.digipres.xena.kernel.XenaException;
 import au.gov.naa.digipres.xena.kernel.XenaInputSource;
 import au.gov.naa.digipres.xena.kernel.filenamer.AbstractFileNamer;
 import au.gov.naa.digipres.xena.kernel.filenamer.FileNamerManager;
 import au.gov.naa.digipres.xena.kernel.normalise.AbstractNormaliser;
-import au.gov.naa.digipres.xena.kernel.normalise.NormaliserManager;
 
 /**
  * Generate a file name for the output file according to NAA policy.
@@ -34,10 +20,6 @@ import au.gov.naa.digipres.xena.kernel.normalise.NormaliserManager;
  */
 public class NaaFileNamer extends AbstractFileNamer {
 
-    private static final String SEP = "\t";
-    
-    private Logger logger = Logger.getLogger(this.getClass().getName());
-    
     private static long timeSeed;
     static {
         timeSeed = System.currentTimeMillis() / 1000;
@@ -55,35 +37,7 @@ public class NaaFileNamer extends AbstractFileNamer {
     public String getName() {
         return "NAA File Namer";
     }
-    
-    
-    
-//    /**
-//     * Make the filename for the new XenaOutputStream.
-//     * This is a fully qualified filename, based on the folders that were specified by the normaliser manager.
-//     * 
-//     * THIS IS RETARDED. WE GET THE DIRECTORY BASED ON THE FILE EXTENSION.
-//     * THIS COMMENT IS _MEANT_ TO BE ALL CAPS.
-//     * 
-//     * Ultimately, this should not be the case.
-//     * 
-//     */
-//    public File makeNewXenaFile(XMLReader normaliser, XenaInputSource input) 
-//    throws XenaException {
-//            File newDir = null;
-////            if (extension.equals(FileNamer.XENA_DEFAULT_EXTENSION)) {
-////                newDir = LegacyXenaCode.getBaseDirectory(NormaliserManager.DESTINATION_DIR_STRING);
-////            } else {
-////                if (extension.equals(FileNamer.XENA_CONFIG_EXTENSION)) {
-////                    newDir = LegacyXenaCode.getBaseDirectory(NormaliserManager.CONFIG_DIR_STRING);
-////                } else {
-////                    throw new XenaException("Unknown extension: " + extension);
-////                }
-////            }
-//            
-//            newDir = fileNamerManager.getDestinationDir();
-//            return makeNewXenaFile(normaliser, input, extension, newDir);
-//    }
+
 
     /**
      * Make the filename for the new XenaOutputStream.
@@ -162,8 +116,8 @@ public class NaaFileNamer extends AbstractFileNamer {
 		return rtn;
 	}
 
-	public FileFilter makeFileFilter(String extension) {
-		return new SimpleFileFilter(extension);
+	public FileFilter makeFileFilter() {
+	    return FileNamerManager.DEFAULT_FILE_FILTER;
 	}
     
     
