1da527ce9c76ac7ea021bf1fbb3c58eab1b5145a
keelia <none@none>
2006-05-15 01:50:25 +0000

    fixed imports.

--- src/au/gov/naa/digipres/xena/util/JdomUtil.java
+++ src/au/gov/naa/digipres/xena/util/JdomUtil.java
@@ -11,12 +11,10 @@ import org.jdom.output.SAXOutputter;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
-import org.xml.sax.XMLFilter;
 import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.XMLFilterImpl;
 
 import au.gov.naa.digipres.xena.kernel.XenaException;
-import au.gov.naa.digipres.xena.kernel.plugin.PluginManager;
 
 /**
  * General utility functions for converting between JDOM and SAX xml streams.
