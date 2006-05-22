46e6a4b9c207f96a2022d160f647ee8b53f2f0dd
jwaddell <none@none>
2006-05-22 05:27:15 +0000

    New build no

--- src/au/gov/naa/digipres/xena/core/ReleaseInfo.java
+++ src/au/gov/naa/digipres/xena/core/ReleaseInfo.java
@@ -1,5 +1,5 @@
 /* Created by JReleaseInfo AntTask from Open Source Competence Group */
-/* Creation date Mon May 22 12:10:15 EST 2006 */
+/* Creation date Mon May 22 13:35:54 EST 2006 */
 package au.gov.naa.digipres.xena.core;
 
 import java.util.Date;
@@ -207,11 +207,11 @@ class JReleaseInfoViewer extends JFrame {
 public class ReleaseInfo {
 
 
-   /** buildDate (set during build process to 1148263815765L). */
-   private static Date buildDate = new Date(1148263815765L);
+   /** buildDate (set during build process to 1148268954187L). */
+   private static Date buildDate = new Date(1148268954187L);
 
    /**
-    * Get buildDate (set during build process to Mon May 22 12:10:15 EST 2006).
+    * Get buildDate (set during build process to Mon May 22 13:35:54 EST 2006).
     * @return Date buildDate
     */
    public static final Date getBuildDate() { return buildDate; }
@@ -235,10 +235,10 @@ public class ReleaseInfo {
 
 
    /**
-    * Get buildNumber (set during build process to 41).
+    * Get buildNumber (set during build process to 42).
     * @return int buildNumber
     */
-   public static final int getBuildNumber() { return 41; }
+   public static final int getBuildNumber() { return 42; }
 
 
    /** version (set during build process to "3.0"). */
