c08a2778360ad83f2e5c5e333c13d9a5bac30853
jwaddell <none@none>
2007-03-01 03:18:49 +0000

    Using InputStreamEncoder for base64 encoding

--- src/au/gov/naa/digipres/xena/kernel/normalise/BinaryToXenaBinaryNormaliser.java
+++ src/au/gov/naa/digipres/xena/kernel/normalise/BinaryToXenaBinaryNormaliser.java
@@ -1,9 +1,13 @@
 package au.gov.naa.digipres.xena.kernel.normalise;
+import java.io.IOException;
 import java.io.InputStream;
 
 import org.xml.sax.ContentHandler;
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 import org.xml.sax.helpers.AttributesImpl;
+
+import au.gov.naa.digipres.xena.util.InputStreamEncoder;
 
 
 /**
@@ -18,48 +22,20 @@ public class BinaryToXenaBinaryNormaliser extends AbstractNormaliser {
 	final static String DESCRIPTION = "The following data is a MIME-compliant (RFC 1421) PEM base64 (RFC 1421) representation of the original file contents.";
 
 	final static String URI = "http://preservation.naa.gov.au/binary-object/1.0";
-
-    /**
-     * RFC suggests max of 76 characters per line
-     */
-    public static final int MAX_BASE64_RFC_LINE_LENGTH = 76;
-
-    /**
-     * Base64 turns 3 characters into 4...
-     */
-    public static final int CHUNK_SIZE = (MAX_BASE64_RFC_LINE_LENGTH * 3) / 4;
 	
 	public String getName() {
 		return "Binary";
 	}
 
-	public void parse(InputSource input, NormaliserResults nr) throws java.io.IOException, org.xml.sax.SAXException {
+	public void parse(InputSource input, NormaliserResults nr) throws IOException, SAXException 
+	{
 		AttributesImpl attributes = new AttributesImpl();
         attributes.addAttribute(URI, PROCESS_DESCRIPTION_TAG_NAME, PROCESS_DESCRIPTION_TAG_NAME, "CDATA", DESCRIPTION);
 		ContentHandler contentHandler = getContentHandler();
-		contentHandler.startElement(URI, PREFIX, PREFIX + ":" + PREFIX, attributes);
-		sun.misc.BASE64Encoder encoder = new sun.misc.BASE64Encoder();
 		InputStream inputStream = input.getByteStream();
 		
-		byte[] readBuffer = new byte[CHUNK_SIZE];
-		int charsRead;
-		while (0 <= (charsRead = inputStream.read(readBuffer))) {
-			byte[] outputBuffer = readBuffer;
-			if (charsRead < readBuffer.length) {
-				outputBuffer = new byte[charsRead];
-				System.arraycopy(readBuffer, 0, outputBuffer, 0, charsRead);
-			}
-			
-			// Encode output with base64 encoding, and write out.
-			// The output needs to be trimmed so we can remove the carriage return character, 
-			// which otherwise gets encoded into the XML
-			char[] encodedChars = encoder.encode(outputBuffer).trim().toCharArray();
-			contentHandler.characters(encodedChars, 0, encodedChars.length);
-			
-			// Print EOL character in order to conform to MIME base64 specification
-			char[] eolCharArr = "\n".toCharArray();
-			contentHandler.characters(eolCharArr, 0, eolCharArr.length);
-		}
+		contentHandler.startElement(URI, PREFIX, PREFIX + ":" + PREFIX, attributes);
+		InputStreamEncoder.base64Encode(inputStream, contentHandler);
 		contentHandler.endElement(URI, PREFIX, PREFIX + ":" + PREFIX);
 	}
 }
