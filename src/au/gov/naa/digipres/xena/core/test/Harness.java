deb93498b0696acbec8571933c5588782ea008fb
keelia <none@none>
2007-03-01 06:23:18 +0000

    commented and cleaned up

--- src/au/gov/naa/digipres/xena/core/test/Harness.java
+++ src/au/gov/naa/digipres/xena/core/test/Harness.java
@@ -4,11 +4,15 @@
  * 
  * Example application to demonstrate normalisation using the Xena API.
  * 
+ * Load plugins from a hard coded plugins folder, create a xena input source
+ * based on a hard coded file name, then use Xena to normalise the file.
+ * Print the success or otherwise of the normalisation process. There is
+ * no error handling with this demonstration tool.
+ *
  */
 package au.gov.naa.digipres.xena.core.test;
 
 import java.io.File;
-
 import au.gov.naa.digipres.xena.core.Xena;
 import au.gov.naa.digipres.xena.kernel.XenaInputSource;
 import au.gov.naa.digipres.xena.kernel.normalise.NormaliserResults;
@@ -21,13 +25,11 @@ public class Harness {
     public static void main(String[] args) {
         Xena xena = new Xena();
         try {
-            
-            // load the plugins named in the list
-            xena.loadPlugins(new File("d://workspace//xena//dist//plugins//plaintext.jar"));
+            // load all the plugins we in the plugins directory
+            xena.loadPlugins(new File("d://workspace//xena//dist//plugins//"));
             
             // create a new XenaInputSource xis based on d:/data/simple.txt file.
-            XenaInputSource xis = null;
-            xis = new XenaInputSource(new File("D:/xena_data/source/simple.txt"));
+            XenaInputSource xis = new XenaInputSource(new File("D:/xena_data/source/simple.txt"));
             
             // create the destination dir – d:/xena_data/destination
             File destDir = new File("d:/xena_data/destination");
@@ -39,8 +41,8 @@ public class Harness {
                 System.out.println("Input normalised: " + results.isNormalised());
                 //ignore the rest of our results for the moment
             }
-            
         } catch (Exception e) {
+        	// Just print a stack trace if any errors
             e.printStackTrace();
         }
     }
