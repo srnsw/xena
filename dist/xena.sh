8b41e3a588055345392d0f9b3994d54b0ac54c24
keelia <none@none>
2007-05-30 03:50:48 +0000

    made class name, which was slightly wrong, really obviously wrong.

---
+++ dist/xena.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Starts the Xena application from the Xena jar file
+
+
+#TODO: Update class name from some java thingimy to the correct one.
+java -cp xena.jar "some java thingimy." -dplugins
