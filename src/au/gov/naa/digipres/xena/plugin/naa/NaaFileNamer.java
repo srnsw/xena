c330176038d2ddb4e02317dae38896514694bba7
jwaddell <none@none>
2007-06-28 07:17:24 +0000

    AIP ID and filename are now in the format timestamp-filename-file path hash

--- src/au/gov/naa/digipres/xena/plugin/naa/NaaFileNamer.java
+++ src/au/gov/naa/digipres/xena/plugin/naa/NaaFileNamer.java
@@ -1,7 +1,11 @@
 package au.gov.naa.digipres.xena.plugin.naa;
 import java.io.File;
 import java.io.FileFilter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Iterator;
 import java.util.List;
 import java.util.zip.CRC32;
@@ -12,6 +16,7 @@ import au.gov.naa.digipres.xena.kernel.XenaInputSource;
 import au.gov.naa.digipres.xena.kernel.filenamer.AbstractFileNamer;
 import au.gov.naa.digipres.xena.kernel.filenamer.FileNamerManager;
 import au.gov.naa.digipres.xena.kernel.normalise.AbstractNormaliser;
+import au.gov.naa.digipres.xena.util.SourceURIParser;
 
 /**
  * Generate a file name for the output file according to NAA policy.
@@ -20,16 +25,10 @@ import au.gov.naa.digipres.xena.kernel.normalise.AbstractNormaliser;
  */
 public class NaaFileNamer extends AbstractFileNamer {
 
-    
+    private static final String TIMESTAMP_FORMAT_STRING = "yyyyMMddHHmmssSSS";
     public static String NAA_FILE_NAMER = "NAA File Namer";
     
-    private static long timeSeed;
-    static {
-        timeSeed = System.currentTimeMillis() / 1000;
-        timeSeed += timeSeed % 2;
-    }
-    
-	public NaaFileNamer() {
+ 	public NaaFileNamer() {
 	}
 
 
@@ -51,72 +50,45 @@ public class NaaFileNamer extends AbstractFileNamer {
     public File makeNewXenaFile(XenaInputSource xis, AbstractNormaliser normaliser, File destinationDir) 
     throws XenaException {
         
-        // lets sort this function out.
-        String id = null;
-        
-        String systemId = xis.getSystemId();
-        
-        // Time to generate file name.
-        
-        //create a list of URLs to feed to our get ID method
-        // (in the case of non-multi source the list just has 1 element!)
-        List<String> inputSourceUrls = new ArrayList<String>();
-        
-        if (xis instanceof MultiInputSource) {
-            MultiInputSource minput = (MultiInputSource)xis;
-            inputSourceUrls.addAll(minput.getSystemIds());
-        } else {
-            inputSourceUrls.add(systemId);
-        }
-        
-        id = getId(inputSourceUrls);
-        
-        
-        assert (id.length() == 16);
-        char lc = id.charAt(15);
-        char[] chs = {lc};
-        int last = Integer.parseInt(new String(chs), 16);
-        assert ((last % 2) == 0);
-        id = id.substring(0, 15) + Integer.toString(last + 1, 16);
-        
-        
+        String id = getId(xis);
+        assert id != null;
+                       
         File newXenaFile = new File(destinationDir, id + "." + FileNamerManager.DEFAULT_EXTENSION);
         return newXenaFile;
-        
     }
 
     
     /**
      * Here is where we actually create an ID.
-     * Finally!
-     * It appears we just use a crc of the fully qualified filename (a uri if you will.)
+     * The ID consists of a timestamp string (date and time, to millisecond accuracy), the original filename and a CRC hash of the full file path.
      * @param urls
      * @return
      */
-	private synchronized static String getId(List urls) {
+	private synchronized static String getId(XenaInputSource xis) 
+	{
 		long idNumber = 0L;
-		Iterator it = urls.iterator();
-		while (it.hasNext()) {
-			String url = (String)it.next();
-            // XOR the current crc32 with the next list item. 
-			CRC32 crc32 = new CRC32();
-            crc32.update(url.getBytes());
-            idNumber ^= crc32.getValue();                
-		}
-		String crcs = Long.toHexString(idNumber);
-		while (crcs.length() < 8) {
-			crcs = "0" + crcs;
-		}
-		assert ((timeSeed % 2) == 0);
-
-		String times = Long.toHexString(timeSeed);
-		timeSeed += 2;
-		while (times.length() < 8) {
-			times = "0" + times;
-		}
-		String rtn = crcs + times;
-		assert (rtn.length() == 16);
-		return rtn;
+		
+		String systemID = xis.getSystemId();
+		
+		// Timestamp
+		Date currentDate = new Date();
+		SimpleDateFormat formatter = new SimpleDateFormat(TIMESTAMP_FORMAT_STRING);
+		String timestampString = formatter.format(currentDate);
+		
+		// Filename
+		String filename = SourceURIParser.getFileNameComponent(xis);
+		
+		// Hash of system ID (full path)
+		CRC32 crc32 = new CRC32();
+        crc32.update(systemID.getBytes());
+        idNumber = crc32.getValue();                
+		String crcStr = Long.toHexString(idNumber);
+		while (crcStr.length() < 8) 
+		{
+			crcStr = "0" + crcStr;
+		}		
+						
+		return timestampString + "-" + filename + "-" + crcStr;
 	}
 
 	public FileFilter makeFileFilter() {
