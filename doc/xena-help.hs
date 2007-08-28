ca5f461f7f5ca42e5c574531ad9692cddf4b89f9
csmart <none@none>
2007-08-28 04:56:10 +0000

    more eyecandy goodness

--- doc/xena-help.hs
+++ doc/xena-help.hs
@@ -1,42 +1,37 @@
-<?xml version='2.0' encoding='ISO-8859-1' ?>
+<?xml version='1.0' encoding='ISO-8859-1' ?>
 <!DOCTYPE helpset
   PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN"
          "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
 
 <helpset version="2.0">
-     <title>Xena Help</title>
-	
-     <maps>
-          <mapref location="map.jhm"/>
-          <homeID>xenahelp</homeID>
-     </maps>
 
-    <view mergetype="javax.help.UniteAppendMerge">
-         <name>TOC</name>
-         <label>TOC</label>
-         <type>javax.help.TOCView</type>
-         <data>toc.xml</data>
-     </view>
+	<title>Xena Help</title>
 
- <!--  Probably no need for a help index for Xena
-	  <view>
-          <name>Index</name>
-          <label>Index</label>
-          <type>javax.help.IndexView</type>
-          <data>index.xml</data>
-     	</view> 
--->
+	<maps>
+		<homeID>xenahelp</homeID>
+		<mapref location="map.jhm"/>
+	</maps>
 
- <!-- Probably no need for a full text search for Xena
-		<view>
-    		<name>Search</name>
-    		<label>Xena Word Search</label>
-    		<type>javax.help.SearchView</type>
-    		<data engine="com.sun.java.help.search.DefaultSearchEngine">
-       		JavaHelpSearch
-  		</data>
-		</view> 
--->
+	<view xml:lang="en" mergetype="javax.help.UniteAppendMerge">
+		<name>TOC</name>
+		<label>Table Of Contents</label>
+		<type>javax.help.TOCView</type>
+		<data>toc.xml</data>
+		<image>contents</image>
+	</view>
+
+	<presentation default=true>
+		<name>Xena Help</name>
+		<size width="800" height="600" />
+		<location x="200" y="200" />
+		<title>Xena Help</title>
+		<toolbar>
+			<helpaction image="back">javax.help.BackAction</helpaction>
+			<helpaction image="forward">javax.help.ForwardAction</helpaction>
+			<helpaction image="home">javax.help.HomeAction</helpaction>
+		</toolbar>
+		<image>titlebaricon</image>
+	</presentation>
 
 </helpset>
   
