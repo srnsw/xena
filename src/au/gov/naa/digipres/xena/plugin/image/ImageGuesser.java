364c9f3023de2ba7a740ccd1a95eded68b6ebcca
jwaddell <none@none>
2008-02-04 22:32:55 +0000

    New Guessing algorithm implemented. All Guessers return an array of FileDescriptors (which can be empty), which contain the various file extentsion and magic number combinations handled by the guesser. These FileDescriptors are loaded into Maps by the GuesserManager. When a file is to be guessed, these maps are first checked for an exact file extension AND magic number match. If both are matched, we can be certain that we have the correct file and thus there is no need to perform the full guessing process for this file.

--- src/au/gov/naa/digipres/xena/plugin/image/ImageGuesser.java
+++ src/au/gov/naa/digipres/xena/plugin/image/ImageGuesser.java
@@ -18,26 +18,27 @@
 
 package au.gov.naa.digipres.xena.plugin.image;
 
-import java.io.IOException;
-
 import au.gov.naa.digipres.xena.kernel.XenaException;
-import au.gov.naa.digipres.xena.kernel.XenaInputSource;
-import au.gov.naa.digipres.xena.kernel.guesser.Guess;
-import au.gov.naa.digipres.xena.kernel.guesser.Guesser;
+import au.gov.naa.digipres.xena.kernel.guesser.DefaultGuesser;
+import au.gov.naa.digipres.xena.kernel.guesser.FileTypeDescriptor;
 import au.gov.naa.digipres.xena.kernel.guesser.GuesserManager;
-import au.gov.naa.digipres.xena.kernel.guesser.GuesserUtils;
 import au.gov.naa.digipres.xena.kernel.type.Type;
 
 /**
  * Guesser for JAI supported image types other than the core JPEG and PNG
  * 
  */
-public class ImageGuesser extends Guesser {
-	static byte[] gifmagic = {'G', 'I', 'F'};
+public class ImageGuesser extends DefaultGuesser {
+	private static final byte[][] gifMagic = {{'G', 'I', 'F'}};
+	private static final String[] gifExtensions = {"gif"};
+	private static final String[] gifMime = {"image/gif"};
 
-	static byte[] gifTail = {0x00, 0x3b};
+	private static final byte[][] bmpMagic = {{'B', 'M'}};
+	private static final String[] bmpExtensions = {"bmp"};
+	private static final String[] bmpMime = {"image/bmp"};
 
-	static byte[] bmpmagic = {'B', 'M'};
+	private FileTypeDescriptor[] descriptorArr =
+	    {new FileTypeDescriptor(gifExtensions, gifMagic, gifMime), new FileTypeDescriptor(bmpExtensions, bmpMagic, bmpMime)};
 
 	private Type type;
 
@@ -50,62 +51,27 @@ public class ImageGuesser extends Guesser {
 	}
 
 	@Override
-	public void initGuesser(GuesserManager guesserManager) throws XenaException {
-		this.guesserManager = guesserManager;
+	public void initGuesser(GuesserManager guesserManagerParam) throws XenaException {
+		guesserManager = guesserManagerParam;
 		type = getTypeManager().lookup(ImageFileType.class);
 	}
 
 	@Override
-    public Guess guess(XenaInputSource source) throws IOException, XenaException {
-		Guess guess = new Guess(type);
-		String type = source.getMimeType();
-
-		// get the mime type...
-		if (type != null && (type.equals("image/gif") || type.equals("image/bmp"))) {
-			guess.setMimeMatch(true);
-		}
-
-		// Get the extension...
-		String id = source.getSystemId().toLowerCase();
-		if (id.endsWith(".gif") || id.endsWith(".bmp")) {
-			guess.setExtensionMatch(true);
-		}
-
-		// Get the magic number
-		byte[] first = new byte[3];
-		source.getByteStream().read(first);
-		if (GuesserUtils.compareByteArrays(first, gifmagic) || GuesserUtils.compareByteArrays(first, bmpmagic)) {
-			guess.setMagicNumber(true);
-
-			// TODO: A better way of checking for data match
-			guess.setDataMatch(true);
-
-		} else {
-			guess.setMagicNumber(false);
-			guess.setPossible(false);
-		}
-
-		return guess;
-	}
-
-	@Override
-    public String getName() {
+	public String getName() {
 		return "ImageGuesser";
-	}
-
-	@Override
-	protected Guess createBestPossibleGuess() {
-		Guess guess = new Guess();
-		guess.setMimeMatch(true);
-		guess.setExtensionMatch(true);
-		guess.setDataMatch(true);
-		guess.setMagicNumber(true);
-		return guess;
 	}
 
 	@Override
 	public Type getType() {
 		return type;
+	}
+
+	/**
+	 * @see au.gov.naa.digipres.xena.kernel.guesser.Guesser#getFileTypeDescriptors()
+	 */
+	@Override
+	protected FileTypeDescriptor[] getFileTypeDescriptors() {
+		return descriptorArr;
 	}
 
 }
