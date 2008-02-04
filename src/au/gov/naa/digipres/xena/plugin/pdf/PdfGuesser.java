e4718743e3abf31090ee39eb03e77721049f63a5
jwaddell <none@none>
2008-02-04 22:47:52 +0000

    New Guessing algorithm implemented. All Guessers return an array of FileDescriptors (which can be empty), which contain the various file extentsion and magic number combinations handled by the guesser. These FileDescriptors are loaded into Maps by the GuesserManager. When a file is to be guessed, these maps are first checked for an exact file extension AND magic number match. If both are matched, we can be certain that we have the correct file and thus there is no need to perform the full guessing process for this file.

--- src/au/gov/naa/digipres/xena/plugin/pdf/PdfGuesser.java
+++ src/au/gov/naa/digipres/xena/plugin/pdf/PdfGuesser.java
@@ -18,81 +18,47 @@
 
 package au.gov.naa.digipres.xena.plugin.pdf;
 
-import java.io.IOException;
-
 import au.gov.naa.digipres.xena.kernel.XenaException;
-import au.gov.naa.digipres.xena.kernel.XenaInputSource;
-import au.gov.naa.digipres.xena.kernel.guesser.Guess;
-import au.gov.naa.digipres.xena.kernel.guesser.Guesser;
+import au.gov.naa.digipres.xena.kernel.guesser.DefaultGuesser;
+import au.gov.naa.digipres.xena.kernel.guesser.FileTypeDescriptor;
 import au.gov.naa.digipres.xena.kernel.guesser.GuesserManager;
-import au.gov.naa.digipres.xena.kernel.guesser.GuesserUtils;
 import au.gov.naa.digipres.xena.kernel.type.Type;
 
 /**
  * Guesser to guess PDF files.
  *
  */
-public class PdfGuesser extends Guesser {
-	static byte[] pdfmagic = {'%', 'P', 'D', 'F'};
+public class PdfGuesser extends DefaultGuesser {
+	private static final byte[][] pdfMagic = {{'%', 'P', 'D', 'F'}};
+	private static final String[] pdfExtensions = {"pdf"};
+	private static final String[] pdfMime = {"application/pdf"};
+
+	private FileTypeDescriptor[] descriptorArr = {new FileTypeDescriptor(pdfExtensions, pdfMagic, pdfMime)};
 
 	private Type type;
 
-	/**
-	 * @throws XenaException 
-	 * 
-	 */
-	public PdfGuesser() {
-		super();
-	}
-
 	@Override
-	public void initGuesser(GuesserManager guesserManager) throws XenaException {
-		this.guesserManager = guesserManager;
+	public void initGuesser(GuesserManager guesserManagerParam) throws XenaException {
+		guesserManager = guesserManagerParam;
 		type = getTypeManager().lookup(PdfFileType.class);
 	}
 
 	@Override
-    public Guess guess(XenaInputSource source) throws IOException, XenaException {
-		Guess rtn = new Guess(type);
-		String type = source.getMimeType();
-		byte[] first = new byte[pdfmagic.length];
-
-		source.getByteStream().read(first);
-		String id = source.getSystemId().toLowerCase();
-		if (type != null && type.equals("application/pdf")) {
-			rtn.setMimeMatch(true);
-		}
-		if (id.endsWith(".pdf")) {
-			rtn.setExtensionMatch(true);
-		}
-		if (GuesserUtils.compareByteArrays(first, pdfmagic)) {
-			rtn.setMagicNumber(true);
-			rtn.setDataMatch(true);
-		} else {
-			rtn.setDataMatch(false);
-			rtn.setPossible(false);
-		}
-		return rtn;
-	}
-
-	@Override
-    public String getName() {
-		return "PDFGuesser";
-	}
-
-	@Override
-	protected Guess createBestPossibleGuess() {
-		Guess guess = new Guess();
-		guess.setMagicNumber(true);
-		guess.setMimeMatch(true);
-		guess.setExtensionMatch(true);
-		guess.setDataMatch(true);
-		return guess;
+	public String getName() {
+		return "PDF Guesser";
 	}
 
 	@Override
 	public Type getType() {
 		return type;
+	}
+
+	/* (non-Javadoc)
+	 * @see au.gov.naa.digipres.xena.kernel.guesser.Guesser#getFileTypeDescriptors()
+	 */
+	@Override
+	protected FileTypeDescriptor[] getFileTypeDescriptors() {
+		return descriptorArr;
 	}
 
 }
