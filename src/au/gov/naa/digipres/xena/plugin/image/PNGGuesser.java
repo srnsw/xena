cea253e34b63288ff840ac0d65a223ba01d3b519
jwaddell <none@none>
2008-02-04 22:32:55 +0000

    New Guessing algorithm implemented. All Guessers return an array of FileDescriptors (which can be empty), which contain the various file extentsion and magic number combinations handled by the guesser. These FileDescriptors are loaded into Maps by the GuesserManager. When a file is to be guessed, these maps are first checked for an exact file extension AND magic number match. If both are matched, we can be certain that we have the correct file and thus there is no need to perform the full guessing process for this file.

--- src/au/gov/naa/digipres/xena/plugin/image/PNGGuesser.java
+++ src/au/gov/naa/digipres/xena/plugin/image/PNGGuesser.java
@@ -22,82 +22,44 @@
  */
 package au.gov.naa.digipres.xena.plugin.image;
 
-import java.io.IOException;
-
 import au.gov.naa.digipres.xena.kernel.XenaException;
-import au.gov.naa.digipres.xena.kernel.XenaInputSource;
-import au.gov.naa.digipres.xena.kernel.guesser.Guess;
-import au.gov.naa.digipres.xena.kernel.guesser.Guesser;
+import au.gov.naa.digipres.xena.kernel.guesser.DefaultGuesser;
+import au.gov.naa.digipres.xena.kernel.guesser.FileTypeDescriptor;
 import au.gov.naa.digipres.xena.kernel.guesser.GuesserManager;
-import au.gov.naa.digipres.xena.kernel.guesser.GuesserUtils;
 import au.gov.naa.digipres.xena.kernel.type.Type;
 
-public class PNGGuesser extends Guesser {
+public class PNGGuesser extends DefaultGuesser {
 
-	static byte[] pngmagic = {new Integer(0x89).byteValue(), 'P', 'N', 'G'};
+	private static final byte[][] pngMagic = {{new Integer(0x89).byteValue(), 'P', 'N', 'G'}};
+	private static final String[] pngExtensions = {"png"};
+	private static final String[] pngMime = {"image/png"};
+
+	private FileTypeDescriptor[] descriptorArr = {new FileTypeDescriptor(pngExtensions, pngMagic, pngMime)};
 
 	private Type type;
 
-	/**
-	 * @throws XenaException 
-	 * 
-	 */
-	public PNGGuesser() {
-		super();
-	}
-
 	@Override
-	public void initGuesser(GuesserManager guesserManager) throws XenaException {
-		this.guesserManager = guesserManager;
+	public void initGuesser(GuesserManager guesserManagerParam) throws XenaException {
+		guesserManager = guesserManagerParam;
 		type = getTypeManager().lookup(PngFileType.class);
 	}
 
 	@Override
-	public Guess guess(XenaInputSource xis) throws XenaException, IOException {
-
-		Guess guess = new Guess(type);
-
-		// Extension and MIME
-		String type = xis.getMimeType();
-		String id = xis.getSystemId().toLowerCase();
-		if (type.equals("image/png")) {
-			guess.setMimeMatch(true);
-		}
-		if (id.endsWith(".png")) {
-			guess.setExtensionMatch(true);
-		}
-
-		// Magic Number
-		byte[] first = new byte[4];
-		xis.getByteStream().read(first);
-		boolean magicMatch = GuesserUtils.compareByteArrays(first, pngmagic);
-		guess.setMagicNumber(magicMatch);
-
-		// Need to find a good way to check for a data match, other than just trying to render it...
-		// Or just using magic number match, as we do at the moment...
-		guess.setDataMatch(magicMatch);
-
-		return guess;
-	}
-
-	@Override
 	public String getName() {
-		return "PNGGuesser";
-	}
-
-	@Override
-	protected Guess createBestPossibleGuess() {
-		Guess guess = new Guess();
-		guess.setMimeMatch(true);
-		guess.setExtensionMatch(true);
-		guess.setDataMatch(true);
-		guess.setMagicNumber(true);
-		return guess;
+		return "PNG Guesser";
 	}
 
 	@Override
 	public Type getType() {
 		return type;
+	}
+
+	/* (non-Javadoc)
+	 * @see au.gov.naa.digipres.xena.kernel.guesser.Guesser#getFileTypeDescriptors()
+	 */
+	@Override
+	protected FileTypeDescriptor[] getFileTypeDescriptors() {
+		return descriptorArr;
 	}
 
 }
