6b4e0e04488dac36b23bc42248e13ec08b607d29
csmart <none@none>
2008-03-31 05:19:39 +0000

    Update help docs to specify OpenOffice 2.x.x as a requirement, rather than 2.2.x (as we are now up to 2.4.0!)

--- src/au/gov/naa/digipres/xena/core/ReleaseInfo.java
+++ src/au/gov/naa/digipres/xena/core/ReleaseInfo.java
@@ -1,28 +1,11 @@
-/**
- * This file is part of Xena.
- * 
- * Xena is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
- * 
- * Xena is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with Xena; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- * 
- * 
- * @author Andrew Keeling
- * @author Chris Bitmead
- * @author Justin Waddell
- */
-
 /* Created by JReleaseInfo AntTask from Open Source Competence Group */
-/* Creation date Mon Jun 18 15:14:06 EST 2007 */
+/* Creation date Mon Mar 31 16:15:19 EST 2008 */
 package au.gov.naa.digipres.xena.core;
 
-import java.util.Date; /*
-						 * $Id$
-						 */
+import java.util.Date;
+/*
+ * $Id$
+ */
 import java.awt.AWTEvent;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
@@ -42,6 +25,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
+
 /**
  * A simple application which uses the generated JReleaseInfo file as
  * argument. It creates a table with the properties in the JReleaseInfo
@@ -50,239 +34,234 @@ import javax.swing.JTable;
  * the Main-Class Manifest-Attribute to this viewer and providing
  * a "double-click view" of relevant build-data.
  *
+ * @author Thomas Cotting, Open Source Competence Group, www.oscg.ch
  */
 class JReleaseInfoViewer extends JFrame {
-	/** ContentPanel. */
-	private JPanel pnlContent = null;
+   /** ContentPanel. */
+   private JPanel pnlContent = null;
 
-	/** ScrollPanel holding table. */
-	private JScrollPane scrlPane = null;
+   /** ScrollPanel holding table. */
+   private JScrollPane scrlPane = null;
 
-	/** Table showing properties. */
-	private JTable tblProps = null;
+   /** Table showing properties. */
+   private JTable tblProps = null;
 
-	/** Status line. */
-	private JLabel lblStatus = null;
+   /** Status line. */
+   private JLabel lblStatus = null;
 
-	/** JReleaseInfo class for introspection. */
-	private Class c = null;
+   /** JReleaseInfo class for introspection. */
+   private Class c = null;
 
-	/** Projectname in title */
-	private String project = null;
+   /** Projectname in title */
+   private String project = null;
 
-	/** Version in title */
-	private String version = null;
+   /** Version in title */
+   private String version = null;
 
-	/**
-	 * Constructor.
-	 * @param c Class 
-	 */
-	public JReleaseInfoViewer(Class c) {
-		super();
-		this.c = c;
-		enableEvents(AWTEvent.WINDOW_EVENT_MASK);
-		initialize();
-	}
+   /**
+    * Constructor.
+    * @param c Class 
+    */
+   public JReleaseInfoViewer(Class c) {
+      super();
+      this.c = c;
+      enableEvents(AWTEvent.WINDOW_EVENT_MASK);
+      initialize();
+   }
 
-	/**
-	 * Center the frame in the middle of the screen.
-	 * @param frame of window
-	 */
-	private void centerFrame(JFrame frame) {
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		Dimension frameSize = frame.getSize();
-		frame.setLocation((screenSize.width / 2) - (frameSize.width / 2), (screenSize.height / 2) - (frameSize.height / 2));
-	}
+   /**
+    * Center the frame in the middle of the screen.
+    * @param frame of window
+    */
+   private void centerFrame(JFrame frame) {
+      Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+      Dimension frameSize = frame.getSize();
+      frame.setLocation((screenSize.width / 2) - (frameSize.width / 2), (screenSize.height / 2) - (frameSize.height / 2));
+   }
 
-	/**
-	 * This method initializes the frame.
-	 *
-	 * @return void
-	 */
-	private void initialize() {
-		setSize(500, 200);
-		centerFrame(this);
+   /**
+    * This method initializes the frame.
+    *
+    * @return void
+    */
+   private void initialize() {
+      setSize(500, 200);
+      centerFrame(this);
 
-		scrlPane = new JScrollPane();
+      scrlPane = new JScrollPane();
 
-		tblProps = getTable();
-		scrlPane.setViewportView(tblProps);
+      tblProps = getTable();
+      scrlPane.setViewportView(tblProps);
 
-		// Status Bar
-		lblStatus = new JLabel();
-		lblStatus.setFont(new Font("SansSerif", Font.PLAIN, 11));
-		lblStatus.setText("JReleaseInfo Viewer by Open Source Competence Group, www.oscg.ch");
+      // Status Bar
+      lblStatus = new JLabel();
+      lblStatus.setFont(new Font("SansSerif", Font.PLAIN, 11));
+      lblStatus.setText("JReleaseInfo Viewer by Open Source Competence Group, www.oscg.ch");
 
-		// Frame
-		pnlContent = new JPanel();
-		pnlContent.setLayout(new BorderLayout());
-		pnlContent.add(scrlPane, BorderLayout.CENTER);
-		pnlContent.add(lblStatus, BorderLayout.SOUTH);
+      // Frame
+      pnlContent = new JPanel();
+      pnlContent.setLayout(new BorderLayout());
+      pnlContent.add(scrlPane, BorderLayout.CENTER);
+      pnlContent.add(lblStatus, BorderLayout.SOUTH);
 
-		this.setContentPane(pnlContent);
+      this.setContentPane(pnlContent);
 
-		// Prepare the title
-		String title = "Release Information";
-		if (project != null) {
-			String prefix = project;
-			if (version != null) {
-				prefix = prefix + " " + version;
-			}
+      // Prepare the title       
+      String title = "Release Information";
+      if (project != null) {
+         String prefix = project;
+         if (version != null) {
+            prefix = prefix + " " + version;
+         }
 
-			this.setTitle(prefix + " - " + title);
-		} else {
-			this.setTitle(title);
-		}
-	}
+         this.setTitle(prefix + " - " + title);
+      } else {
+         this.setTitle(title);
+      }
+   }
 
-	/**
-	 * Create a table with two columns.
-	 * @return table
-	 */
-	private JTable getTable() {
-		JTable tbl = null;
-		try {
-			Vector cols = new Vector();
-			cols.add("Property");
-			cols.add("Value");
-			Vector rows = new Vector();
+   /**
+    * Create a table with two columns.
+    * @return table
+    */
+   private JTable getTable() {
+      JTable tbl = null;
+      try {
+         Vector cols = new Vector();
+         cols.add("Property");
+         cols.add("Value");
+         Vector rows = new Vector();
 
-			Object obj = c.newInstance();
-			Method[] methods = c.getDeclaredMethods();
+         Object obj = c.newInstance();
+         Method[] methods = c.getDeclaredMethods();
 
-			// Feature request from rgisler 20040430
-			Map sortedProps = new TreeMap();
+         // Feature request from rgisler 20040430
+         Map sortedProps = new TreeMap();
 
-			for (int i = 0; i < methods.length; i++) {
-				Vector v = new Vector();
-				Method method = methods[i];
-				String methodName = method.getName();
+         for (int i = 0; i < methods.length; i++) {
+            Vector v = new Vector();
+            Method method = methods[i];
+            String methodName = method.getName();
 
-				// Feature request from rgisler 20040430
-				if (methodName.equalsIgnoreCase("getProject")) {
-					Object objP = method.invoke(obj, null);
-					if (objP instanceof String) {
-						project = (String) objP;
-					}
-				}
+            // Feature request from rgisler 20040430
+            if (methodName.equalsIgnoreCase("getProject")) {
+               Object objP = method.invoke(obj, null);
+               if (objP instanceof String) {
+                  project = (String)objP;
+               }
+            }
 
-				if (methodName.equalsIgnoreCase("getVersion")) {
-					Object objV = method.invoke(obj, null);
-					if (objV instanceof String) {
-						version = (String) objV;
-					} else {
-						version = objV.toString();
-					}
-				}
+            if (methodName.equalsIgnoreCase("getVersion")) {
+               Object objV = method.invoke(obj, null);
+               if (objV instanceof String) {
+                  version = (String)objV;
+               } else {
+                  version = objV.toString();
+               }
+            }
 
-				if (methodName.startsWith("get")) {
-					String field = methodName.substring(3);
-					v.add(field);
-					v.add(method.invoke(obj, null));
-					sortedProps.put(field, v);
-				} else if (methodName.startsWith("is")) {
-					String field = methodName.substring(2);
-					v.add(field);
-					v.add(method.invoke(obj, null));
-					sortedProps.put(field, v);
-				}
-			}
+            if (methodName.startsWith("get")) {
+               String field = methodName.substring(3);
+               v.add(field);
+               v.add(method.invoke(obj, null));
+               sortedProps.put(field, v);
+            } else if (methodName.startsWith("is")) {
+               String field = methodName.substring(2);
+               v.add(field);
+               v.add(method.invoke(obj, null));
+               sortedProps.put(field, v);
+            }
+         }
 
-			rows.addAll(sortedProps.values());
+         rows.addAll(sortedProps.values());
 
-			tbl = new JTable(rows, cols);
-			tbl.getColumn("Property").setPreferredWidth(150);
-			tbl.getColumn("Value").setPreferredWidth(350);
-			tbl.setEnabled(false);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+         tbl = new JTable(rows, cols);
+         tbl.getColumn("Property").setPreferredWidth(150);
+         tbl.getColumn("Value").setPreferredWidth(350);
+         tbl.setEnabled(false);
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
 
-		return tbl;
-	}
+      return tbl;
+   }
 
-	/**
-	 * Process Window close event.
-	 * @param e WindowEvent
-	 */
-	@Override
-    protected void processWindowEvent(WindowEvent e) {
-		super.processWindowEvent(e);
-		if (e.getID() == WindowEvent.WINDOW_CLOSING) {
-			System.exit(0);
-		}
-	}
+   /**
+    * Process Window close event.
+    * @param e WindowEvent
+    */
+   protected void processWindowEvent(WindowEvent e) {
+      super.processWindowEvent(e);
+      if (e.getID() == WindowEvent.WINDOW_CLOSING) {
+         System.exit(0);
+      }
+   }
 
 }
 
 /**
  * This class provides information gathered from the build environment.
  * 
+ * @author JReleaseInfo AntTask
  */
 public class ReleaseInfo {
 
-	/** buildDate (set during build process to 1182143646502L). */
-	private static Date buildDate = new Date(1182143646502L);
 
-	/**
-	 * Get buildDate (set during build process to Mon Jun 18 15:14:06 EST 2007).
-	 * @return Date buildDate
-	 */
-	public static final Date getBuildDate() {
-		return buildDate;
-	}
+   /** buildDate (set during build process to 1206940519502L). */
+   private static Date buildDate = new Date(1206940519502L);
 
-	/**
-	 * Get versionNum (set during build process to 3).
-	 * @return int versionNum
-	 */
-	public static final int getVersionNum() {
-		return 3;
-	}
+   /**
+    * Get buildDate (set during build process to Mon Mar 31 16:15:19 EST 2008).
+    * @return Date buildDate
+    */
+   public static final Date getBuildDate() { return buildDate; }
 
-	/** revisionNum (set during build process to 0). */
-	private static Integer revisionNum = new Integer(0);
 
-	/**
-	 * Get revisionNum (set during build process to 0).
-	 * @return Integer revisionNum
-	 */
-	public static final Integer getRevisionNum() {
-		return revisionNum;
-	}
+   /**
+    * Get versionNum (set during build process to 4).
+    * @return int versionNum
+    */
+   public static final int getVersionNum() { return 4; }
 
-	/**
-	 * Get buildNumber (set during build process to 74).
-	 * @return int buildNumber
-	 */
-	public static final int getBuildNumber() {
-		return 74;
-	}
 
-	/** version (set during build process to "3.0"). */
-	private static String version = new String("3.0");
+   /** revisionNum (set during build process to 1). */
+   private static Integer revisionNum = new Integer(1);
 
-	/**
-	 * Get version (set during build process to "3.0").
-	 * @return String version
-	 */
-	public static final String getVersion() {
-		return version;
-	}
+   /**
+    * Get revisionNum (set during build process to 1).
+    * @return Integer revisionNum
+    */
+   public static final Integer getRevisionNum() { return revisionNum; }
 
-	/** project (set during build process to "Xena"). */
-	private static String project = new String("Xena");
 
-	/**
-	 * Get project (set during build process to "Xena").
-	 * @return String project
-	 */
-	public static final String getProject() {
-		return project;
-	}
+   /**
+    * Get buildNumber (set during build process to 1).
+    * @return int buildNumber
+    */
+   public static final int getBuildNumber() { return 1; }
 
-	public static void main(String[] args) throws Exception {
-		JReleaseInfoViewer frame = new JReleaseInfoViewer(ReleaseInfo.class);
-		frame.setVisible(true);
-	}
+
+   /** version (set during build process to "4.1"). */
+   private static String version = new String("4.1");
+
+   /**
+    * Get version (set during build process to "4.1").
+    * @return String version
+    */
+   public static final String getVersion() { return version; }
+
+
+   /** project (set during build process to "Xena"). */
+   private static String project = new String("Xena");
+
+   /**
+    * Get project (set during build process to "Xena").
+    * @return String project
+    */
+   public static final String getProject() { return project; }
+
+   public static void main(String[] args) throws Exception {
+      JReleaseInfoViewer frame = new JReleaseInfoViewer(ReleaseInfo.class);
+      frame.setVisible(true);
+   }
 }
