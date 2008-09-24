df333d84f0aff225cab2cdf6325e624ea2ea6c36
jwaddell <none@none>
2008-09-24 06:11:28 +0000

    Validating sleep time as a number between 0 and 300

--- src/au/gov/naa/digipres/xena/plugin/office/OfficeProperties.java
+++ src/au/gov/naa/digipres/xena/plugin/office/OfficeProperties.java
@@ -1,14 +1,14 @@
 /**
  * This file is part of Xena.
  * 
- * Xena is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+ * Xena is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later version.
  * 
- * Xena is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * Xena is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License along with Xena; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * You should have received a copy of the GNU General Public License along with Xena; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
  * 
  * 
  * @author Andrew Keeling
@@ -18,7 +18,6 @@
 
 /*
  * Created on 14/02/2006 justinw5
- * 
  */
 package au.gov.naa.digipres.xena.plugin.office;
 
@@ -63,11 +62,11 @@ public class OfficeProperties extends PluginProperties {
 
 		// Office location
 		XenaProperty locationProperty =
-		    new XenaProperty(OOO_DIR_PROP_NAME, "Base directory of OpenOffice installation", XenaProperty.PropertyType.DIR_TYPE, this.getName()) {
+		    new XenaProperty(OOO_DIR_PROP_NAME, "Base directory of OpenOffice installation", XenaProperty.PropertyType.DIR_TYPE, getName()) {
 
 			    /**
-			     * Perform basic validation. Subclasses should call this method in
-			     * addition to any specific validation which may be required.
+			     * Validates that the chosen value for the OpenOffice base directory is not null or empty,
+			     * and the path represents an existing directory.
 			     * 
 			     * @param newValue
 			     * @throws InvalidPropertyException
@@ -75,38 +74,58 @@ public class OfficeProperties extends PluginProperties {
 			     */
 			    @Override
 			    public void validate(String newValue) throws InvalidPropertyException, PropertyMessageException {
-				    if (newValue == null) {
-					    throw new InvalidPropertyException("New value for property " + getName() + " is null");
-				    }
-				    if (newValue instanceof String) {
-					    String valueString = getName();
-					    if (valueString.length() == 0) {
-						    throw new InvalidPropertyException("New value for property " + getName() + " is null");
-					    }
-				    } else {
-					    throw new InvalidPropertyException("New value for property " + getName() + " is not a string!");
+				    super.validate(newValue);
+				    if (newValue == null || newValue.length() == 0) {
+					    throw new InvalidPropertyException("New value for property " + getName() + " is null or empty");
 				    }
 
 				    File location = new File(newValue);
-				    if (location == null || !location.exists() || !location.isDirectory()) {
-					    throw new InvalidPropertyException("Invalid location for open office!");
+				    if (!location.exists() || !location.isDirectory()) {
+					    throw new InvalidPropertyException("Invalid base directory for open office!");
 				    }
 			    }
 		    };
 
-		this.getManager().loadProperty(locationProperty);
+		getManager().loadProperty(locationProperty);
 		properties.add(locationProperty);
 
 		// Office startup sleep time
 		XenaProperty sleepProperty =
-		    new XenaProperty(OOO_SLEEP_PROP_NAME, "Sleep time allowed for OpenOffice to start (seconds)", XenaProperty.PropertyType.INT_TYPE, this
-		            .getName());
-		this.getManager().loadProperty(sleepProperty);
+		    new XenaProperty(OOO_SLEEP_PROP_NAME, "Sleep time allowed for OpenOffice to start (seconds)", XenaProperty.PropertyType.INT_TYPE,
+		                     getName()) {
+
+			    /**
+			     * Validates that the chosen value for the OpenOffice sleep time is a valid number from 0 to 300 (5 minutes)
+			     * 
+			     * @param newValue
+			     * @throws InvalidPropertyException
+			     * @throws PropertyMessageException 
+			     */
+			    @Override
+			    public void validate(String newValue) throws InvalidPropertyException, PropertyMessageException {
+				    super.validate(newValue);
+				    if (newValue == null || newValue.length() == 0) {
+					    throw new InvalidPropertyException("New value for property " + getName() + " is null or empty");
+				    }
+
+				    try {
+					    int newSleepTime = Integer.parseInt(newValue);
+					    if (newSleepTime < 0 || newSleepTime > 300) {
+						    throw new InvalidPropertyException("New value for property " + name + " must be between 0 and 300.");
+					    }
+				    } catch (NumberFormatException e) {
+					    throw new InvalidPropertyException("New value for property " + name + " is not a valid number", e);
+				    }
+			    }
+
+		    };
+
+		getManager().loadProperty(sleepProperty);
 
 		// If first use, set default sleep time
-		if (sleepProperty != null && sleepProperty.getValue().equals("")) {
+		if (sleepProperty.getValue().equals("")) {
 			sleepProperty.setValue(OOO_SLEEP_DEFAULT_VALUE);
-			this.getManager().saveProperty(sleepProperty);
+			getManager().saveProperty(sleepProperty);
 		}
 
 		properties.add(sleepProperty);
