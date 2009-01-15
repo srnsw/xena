0ddcce7255c73b971a59d20bfec2669f14488000
jwaddell <none@none>
2009-01-15 04:47:40 +0000

    All file types now have their own FileType subclass and their own Guesser, so that they can be given accurate mime types.

---
+++ src/au/gov/naa/digipres/xena/plugin/office/wordprocessor/OdtGuesser.java
@@ -0,0 +1,72 @@
+/**
+ * This file is part of Xena.
+ * 
+ * Xena is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+ * 
+ * Xena is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with Xena; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * 
+ * @author Andrew Keeling
+ * @author Chris Bitmead
+ * @author Justin Waddell
+ */
+
+package au.gov.naa.digipres.xena.plugin.office.wordprocessor;
+
+import au.gov.naa.digipres.xena.kernel.XenaException;
+import au.gov.naa.digipres.xena.kernel.guesser.DefaultGuesser;
+import au.gov.naa.digipres.xena.kernel.guesser.FileTypeDescriptor;
+import au.gov.naa.digipres.xena.kernel.guesser.GuesserManager;
+import au.gov.naa.digipres.xena.kernel.type.Type;
+
+/**
+ * Guesser for the ODT file type (ODF word processor format in later versions of OpenOffice)
+ * 
+ */
+public class OdtGuesser extends DefaultGuesser {
+	private static byte[][] odtMagic = {{0x50, 0x4B, 0x03, 0x04}};
+	private static final String[] odtExtensions = {"odt"};
+	private static final String[] odtMime = {"application/vnd.oasis.opendocument.text"};
+
+	private FileTypeDescriptor[] descriptorArr = {new FileTypeDescriptor(odtExtensions, odtMagic, odtMime)};
+
+	private Type type;
+
+	/**
+	 * @throws XenaException 
+	 * 
+	 */
+	public OdtGuesser() {
+		super();
+	}
+
+	@Override
+	public void initGuesser(GuesserManager guesserManagerParam) throws XenaException {
+		guesserManager = guesserManagerParam;
+		type = getTypeManager().lookup(OdtFileType.class);
+	}
+
+	@Override
+	public String getName() {
+		return "ODT Guesser";
+	}
+
+	@Override
+	public Type getType() {
+		return type;
+	}
+
+	/**
+	 * @see au.gov.naa.digipres.xena.kernel.guesser.Guesser#getFileTypeDescriptors()
+	 */
+	@Override
+	protected FileTypeDescriptor[] getFileTypeDescriptors() {
+		return descriptorArr;
+	}
+
+}
