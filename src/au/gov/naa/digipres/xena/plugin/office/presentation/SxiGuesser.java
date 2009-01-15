cc6a2dde86939c51f216cad145ffc24fac7ab933
jwaddell <none@none>
2009-01-15 04:47:40 +0000

    All file types now have their own FileType subclass and their own Guesser, so that they can be given accurate mime types.

---
+++ src/au/gov/naa/digipres/xena/plugin/office/presentation/SxiGuesser.java
@@ -0,0 +1,72 @@
+/**
+ * This file is part of Xena.
+ * 
+ * Xena is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+ * 
+ * Xena is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with Xena; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * 
+ * @author Andrew Keeling
+ * @author Chris Bitmead
+ * @author Justin Waddell
+ */
+
+package au.gov.naa.digipres.xena.plugin.office.presentation;
+
+import au.gov.naa.digipres.xena.kernel.XenaException;
+import au.gov.naa.digipres.xena.kernel.guesser.DefaultGuesser;
+import au.gov.naa.digipres.xena.kernel.guesser.FileTypeDescriptor;
+import au.gov.naa.digipres.xena.kernel.guesser.GuesserManager;
+import au.gov.naa.digipres.xena.kernel.type.Type;
+
+/**
+ * Guesser for the SXI file type (presentation format in early versions of OpenOffice)
+ * 
+ */
+public class SxiGuesser extends DefaultGuesser {
+	private static byte[][] sxiMagic = {};
+	private static final String[] sxiExtensions = {"sxi"};
+	private static final String[] sxiMime = {"application/vnd.sun.xml.impress"};
+
+	private FileTypeDescriptor[] descriptorArr = {new FileTypeDescriptor(sxiExtensions, sxiMagic, sxiMime)};
+
+	private Type type;
+
+	/**
+	 * @throws XenaException 
+	 * 
+	 */
+	public SxiGuesser() {
+		super();
+	}
+
+	@Override
+	public void initGuesser(GuesserManager guesserManagerParam) throws XenaException {
+		guesserManager = guesserManagerParam;
+		type = getTypeManager().lookup(SxiFileType.class);
+	}
+
+	@Override
+	public String getName() {
+		return "SXI Guesser";
+	}
+
+	@Override
+	public Type getType() {
+		return type;
+	}
+
+	/**
+	 * @see au.gov.naa.digipres.xena.kernel.guesser.Guesser#getFileTypeDescriptors()
+	 */
+	@Override
+	protected FileTypeDescriptor[] getFileTypeDescriptors() {
+		return descriptorArr;
+	}
+
+}
---
+++ src/au/gov/naa/digipres/xena/plugin/office/wordprocessor/DocxFileType.java
@@ -0,0 +1,52 @@
+/**
+ * This file is part of Xena.
+ * 
+ * Xena is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+ * 
+ * Xena is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with Xena; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * 
+ * @author Andrew Keeling
+ * @author Chris Bitmead
+ * @author Justin Waddell
+ */
+
+package au.gov.naa.digipres.xena.plugin.office.wordprocessor;
+
+import au.gov.naa.digipres.xena.plugin.office.OfficeFileType;
+
+/**
+ * Type to represent an OOXML Word Document
+ *
+ */
+public class DocxFileType extends OfficeFileType {
+	@Override
+	public String getName() {
+		return "OOXML Document";
+	}
+
+	@Override
+	public String getMimeType() {
+		return "application/word";
+	}
+
+	@Override
+	public String getOfficeConverterName() {
+		return "writer8";
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see au.gov.naa.digipres.xena.kernel.type.FileType#fileExtension()
+	 */
+	@Override
+	public String fileExtension() {
+		return "docx";
+	}
+
+}
