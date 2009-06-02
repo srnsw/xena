e2a3179b06abe146d8d1771a28b1befb41055670
jwaddell <none@none>
2009-06-02 07:26:22 +0000

    Changed references from "searchable" to "text".

--- src/au/gov/naa/digipres/xena/plugin/office/wordprocessor/WordFileType.java
+++ src/au/gov/naa/digipres/xena/plugin/office/wordprocessor/WordFileType.java
@@ -54,10 +54,10 @@ public class WordFileType extends OfficeFileType {
 	}
 
 	/* (non-Javadoc)
-	 * @see au.gov.naa.digipres.xena.plugin.office.OfficeFileType#getSearchableConverterName()
+	 * @see au.gov.naa.digipres.xena.plugin.office.OfficeFileType#getTextConverterName()
 	 */
 	@Override
-	public String getSearchableConverterName() {
+	public String getTextConverterName() {
 		return "Text";
 	}
 
