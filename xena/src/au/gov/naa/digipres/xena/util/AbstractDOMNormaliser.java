f3099db8bda563fc5d0df52df1fdc850f1b40919
jeff <jeff@smeehee.dyndns.org>
2011-04-19 09:39:56 +1000

    New Feature: Widespread modifications across Xena and all plugins to facilitate the splitting of the Conversion and Normalisation of files within Xena.
    This change allows for files that Xena is able to convert to an Open Format to just be converted to
    that format rather than being placed within a Xena file.

--- xena/src/au/gov/naa/digipres/xena/util/AbstractDOMNormaliser.java
+++ xena/src/au/gov/naa/digipres/xena/util/AbstractDOMNormaliser.java
@@ -2,7 +2,7 @@
  * This file is part of Xena.
  * 
  * Xena is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+ * published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
  * 
  * Xena is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
@@ -15,6 +15,7 @@
  * @author Chris Bitmead
  * @author Justin Waddell
  * @author Matthew Oliver
+ * @author Jeff Stiff
  */
 
 package au.gov.naa.digipres.xena.util;
@@ -37,7 +38,7 @@ import au.gov.naa.digipres.xena.kernel.normalise.NormaliserResults;
  */
 abstract public class AbstractDOMNormaliser extends AbstractNormaliser {
 	@Override
-	public void parse(InputSource input, NormaliserResults results) throws java.io.IOException, org.xml.sax.SAXException {
+	public void parse(InputSource input, NormaliserResults results, boolean migrateOnly) throws java.io.IOException, org.xml.sax.SAXException {
 		// Call the JDOM normalisation procedure
 		Element el = normalise(input);
 		// Now write the tree as if it was SAX events.
