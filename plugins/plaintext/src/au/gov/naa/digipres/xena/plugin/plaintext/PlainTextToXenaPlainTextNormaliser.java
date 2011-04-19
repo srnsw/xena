8a5a6998f91409d3789fef23cad81e23c6a48b4e
jeff <jeff@smeehee.dyndns.org>
2011-04-19 11:02:54 +1000

    New Feature: Widespread modifications across Xena and all plugins to facilitate the splitting of the Conversion and Normalisation of files within Xena.
    This change allows for files that Xena is able to convert to an Open Format to just be converted to
    that format rather than being placed within a Xena file.

--- plugins/plaintext/src/au/gov/naa/digipres/xena/plugin/plaintext/PlainTextToXenaPlainTextNormaliser.java
+++ plugins/plaintext/src/au/gov/naa/digipres/xena/plugin/plaintext/PlainTextToXenaPlainTextNormaliser.java
@@ -2,7 +2,7 @@
  * This file is part of Xena.
  * 
  * Xena is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+ * published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
  * 
  * Xena is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
@@ -14,6 +14,7 @@
  * @author Andrew Keeling
  * @author Chris Bitmead
  * @author Justin Waddell
+ * @author Jeff Stiff
  */
 
 package au.gov.naa.digipres.xena.plugin.plaintext;
@@ -65,7 +66,7 @@ public class PlainTextToXenaPlainTextNormaliser extends AbstractNormaliser {
 	}
 
 	@Override
-	public void parse(InputSource input, NormaliserResults results) throws java.io.IOException, org.xml.sax.SAXException {
+	public void parse(InputSource input, NormaliserResults results, boolean migrateOnly) throws java.io.IOException, org.xml.sax.SAXException {
 		InputStream inputStream = input.getByteStream();
 		inputStream.mark(Integer.MAX_VALUE);
 		if (input.getEncoding() == null) {
@@ -76,8 +77,8 @@ public class PlainTextToXenaPlainTextNormaliser extends AbstractNormaliser {
 		AttributesImpl topAttribute = new AttributesImpl();
 		AttributesImpl attribute = new AttributesImpl();
 		tabSizeString =
-		    normaliserManager.getPluginManager().getPropertiesManager().getPropertyValue(PlainTextProperties.PLUGIN_NAME,
-		                                                                                 PlainTextProperties.TAB_SIZE);
+		    normaliserManager.getPluginManager().getPropertiesManager()
+		            .getPropertyValue(PlainTextProperties.PLUGIN_NAME, PlainTextProperties.TAB_SIZE);
 
 		if (tabSizeString != null) {
 			topAttribute.addAttribute(URI, "tabsize", PREFIX + ":" + "tabsize", "CDATA", tabSizeString.toString());
@@ -159,6 +160,16 @@ public class PlainTextToXenaPlainTextNormaliser extends AbstractNormaliser {
 	@Override
 	public String getVersion() {
 		return ReleaseInfo.getVersion() + "b" + ReleaseInfo.getBuildNumber();
+	}
+
+	@Override
+	public boolean isConvertible() {
+		return false;
+	}
+
+	@Override
+	public String getOutputFileExtension() {
+		return "txt";
 	}
 
 }
