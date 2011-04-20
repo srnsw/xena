677b0d19c58dc1152935f82d1b784a4348f920a0
jeff <jeff@smeehee.dyndns.org>
2011-04-20 17:40:39 +1000

    New Feature: Widespread modifications across Xena and all plugins to facilitate the splitting of the Conversion and Normalisation of files within Xena.
    This change allows for files that Xena is able to convert to an Open Format to just be converted to
    that format rather than being placed within a Xena file.

--- plugins/project/src/au/gov/naa/digipres/xena/plugin/project/MsProjectToXenaProjectNormaliser.java
+++ plugins/project/src/au/gov/naa/digipres/xena/plugin/project/MsProjectToXenaProjectNormaliser.java
@@ -2,7 +2,7 @@
  * This file is part of Xena.
  * 
  * Xena is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+ * published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
  * 
  * Xena is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
@@ -15,11 +15,13 @@
  * @author Chris Bitmead
  * @author Justin Waddell
  * @author Matthew Oliver
+ * @author Jeff Stiff
  */
 
 package au.gov.naa.digipres.xena.plugin.project;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -39,6 +41,7 @@ import au.gov.naa.digipres.xena.kernel.ByteArrayInputSource;
 import au.gov.naa.digipres.xena.kernel.XenaInputSource;
 import au.gov.naa.digipres.xena.kernel.normalise.AbstractNormaliser;
 import au.gov.naa.digipres.xena.kernel.normalise.NormaliserResults;
+import au.gov.naa.digipres.xena.util.FileUtils;
 
 /**
  * Normalise Ms Project files to Xena Project files. For pragmatic reasons, the
@@ -53,7 +56,7 @@ public class MsProjectToXenaProjectNormaliser extends AbstractNormaliser {
 	}
 
 	@Override
-	public void parse(InputSource input, NormaliserResults results) throws IOException, SAXException {
+	public void parse(InputSource input, NormaliserResults results, boolean migrateOnly) throws IOException, SAXException {
 		ProjectFile projFile = null;
 		try {
 			MPPReader mppReader = new MPPReader();
@@ -64,28 +67,36 @@ public class MsProjectToXenaProjectNormaliser extends AbstractNormaliser {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		MSPDIWriter mspdiWriter = new MSPDIWriter();
 		mspdiWriter.write(projFile, baos);
-		XenaInputSource is = new ByteArrayInputSource(baos.toByteArray(), null);
-		XMLReader reader = null;
-		try {
-			reader = SAXParserFactory.newInstance().newSAXParser().getXMLReader();
-		} catch (ParserConfigurationException x) {
-			throw new SAXException(x);
-		}
-		XMLFilterImpl filter = new XMLFilterImpl(reader) {
-			// Without this our doc gets meta-data wrapped twice.
-			@Override
-			public void endDocument() {
-				// Do nothing
-			}
 
-			@Override
-			public void startDocument() {
-				// Do nothing
+		if (migrateOnly) {
+			// Copy the ByteArray to the output file
+			FileUtils.fileCopy(baos.toByteArray(), results.getDestinationDirString() + File.separator + results.getOutputFileName(), true);
+		} else {
+			// Process into the Xena file
+			XenaInputSource is = new ByteArrayInputSource(baos.toByteArray(), null);
+
+			XMLReader reader = null;
+			try {
+				reader = SAXParserFactory.newInstance().newSAXParser().getXMLReader();
+			} catch (ParserConfigurationException x) {
+				throw new SAXException(x);
 			}
-		};
-		filter.setContentHandler(getContentHandler());
-		reader.setContentHandler(filter);
-		reader.parse(is);
+			XMLFilterImpl filter = new XMLFilterImpl(reader) {
+				// Without this our doc gets meta-data wrapped twice.
+				@Override
+				public void endDocument() {
+					// Do nothing
+				}
+
+				@Override
+				public void startDocument() {
+					// Do nothing
+				}
+			};
+			filter.setContentHandler(getContentHandler());
+			reader.setContentHandler(filter);
+			reader.parse(is);
+		}
 	}
 
 	@Override
@@ -93,4 +104,13 @@ public class MsProjectToXenaProjectNormaliser extends AbstractNormaliser {
 		return ReleaseInfo.getVersion() + "b" + ReleaseInfo.getBuildNumber();
 	}
 
+	@Override
+	public boolean isConvertible() {
+		return true;
+	}
+
+	@Override
+	public String getOutputFileExtension() {
+		return "xml";
+	}
 }
