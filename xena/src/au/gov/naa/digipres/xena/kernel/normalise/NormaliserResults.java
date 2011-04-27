855b2b73e59ccb035a86b1e1d4e7e1da39ff9774
jeff <jeff@smeehee.dyndns.org>
2011-04-27 11:07:55 +1000

    Trivial comments cleanup including GPL version update

--- xena/src/au/gov/naa/digipres/xena/kernel/normalise/NormaliserResults.java
+++ xena/src/au/gov/naa/digipres/xena/kernel/normalise/NormaliserResults.java
@@ -2,7 +2,7 @@
  * This file is part of Xena.
  * 
  * Xena is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+ * published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
  * 
  * Xena is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
@@ -158,7 +158,7 @@ public class NormaliserResults {
 	}
 
 	/**
-	 * Construtcor containing values to set results to. Fields still initialised
+	 * Constructor containing values to set results to. Fields still initialised
 	 * as required.
 	 * 
 	 * @param xis
